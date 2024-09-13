a33e553b8b9ef2ff4fe48de41f9333c4cbf821c1
Priscila Romano <161603421+PriRomano@users.noreply.github.com>
2024-09-13 10:15:16 -0300

    Aula dia 13 setembro

--- Aula02JS_ID/app.js
+++ Aula02JS_ID/app.js
@@ -1,3 +1,5 @@
+// para rodar o código, vai no terminal e digita - node app.js
+
 console.log("Olá Mundo");
 console.log("Hi world");
 
@@ -104,8 +106,71 @@ console.log(frutas_)
 frutas_.splice(2, 1, 'manga') // o primeiro número é o número do vetor (a posição dele), o segundo é o número de elementos que vai ser substituídos
 console.log(frutas_)
 
+// verificando se todos os elementos existem
+let existe = frutas.includes('morango')
+console.log(existe);
 
-let r = frutas_.includes('morango')
-let r1 = frutas_.includes('morangooo')
-console.log(r, r1)
+// let r = frutas_.includes('morango')
+// let r1 = frutas_.includes('morangooo')
+// console.log(r, r1)
+
+// adiciona todos os elementos em uma string separados por um delimitador escolhido
+let frutas2 = ['laranja', 'melância', 'morango', 'uva', 'caju'];
+let texto = frutas2.join(' , ');
+console.log(texto);
+
+
+// usado para exibir o resultado de uma função
+let numeros = [1, 2, 3, 4, 5, 6];
+let dobra = numeros.map(x=> x*2);
+console.log(dobra);
+
+// exemplo de filter (numeros pares = 0 / numeros impares = 1)
+// validação lógica
+let pares = numeros.filter(x=> x % 2 ==0);
+console.log(pares);
+
+// função find, retorna o valor do primeiro elemento que satisfaz a função fornecida
+let localiza_elemento = numeros.find(x => x > 2);
+let localiza_index = numeros.findIndex(x=> x>2)
+
+console.log("Foi localizado o elemento: " +localiza_elemento)
+console.log("Foi localizado o index: " +localiza_index)
+
+let maior = numeros.some(x=> x>12);
+console.log("Atende a condição lógica: "+maior);
+
+let r = numeros.every(x=> x>0)
+console.log("Todos atendem a regra: " +r)
+
+// organizar os elementos
+let n_aleatorio = [5,3,1,0,6,2,8,7]
+console.log(n_aleatorio) // ordem aleatoria original
+n_aleatorio.sort()
+console.log(n_aleatorio) // na ordem do menor p maior
+n_aleatorio.reverse()
+console.log(n_aleatorio) // na ordem do maior p menor
+
+
+
+
+// ternario (if-else simplificado em uma linha)
+// exemplo aluno aprovado ou não
+let nota = 6
+let res = nota>=6 ? "Aprovado":"Reprovado"
+
+// ou
+let res1 =6;
+if (nota>=6){
+    console.log ("Aprovado")
+} else {
+    console.log ("Reprovado")
+}
+
+
+
+// exemplo de função
+function teste (t){
+    console.log(t)
+}
 
